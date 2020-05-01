a12674fbe252d57175021c05b1b1bf6aa69a6786
Alex Kelly <alex.kelly@franklin.edu>
2020-05-01 15:29:05 -0400

    rewrote get_all_projects to parse json data from task command

--- taskwarrior.py
+++ taskwarrior.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python3
 
 import subprocess
+import json
 from flask import Flask, render_template
 
 
 class Task:
-    def get_projects(self):
-        projoutput = subprocess.run(['task', '_projects'],
-                                    stdout=subprocess.PIPE)
-        projects = projoutput.stdout.splitlines()
-        returnprojects = []
-        for project in projects:
-            returnprojects.append(project.decode())
-        return returnprojects
+    projoutput = subprocess.run(['task', 'status:pending', 'export'],
+                                stdout=subprocess.PIPE)
+    alltasks = json.loads(projoutput.stdout)
+
+    def get_all_projects(self):
+        alltasks = self.alltasks
+        allprojects = []
+        iter = 0
+        maxiter = len(alltasks)
+        while iter < maxiter:
+            try:
+                print(alltasks[iter]['project'])
+                if alltasks[iter]['project'] not in allprojects:
+                   # if '.' in alltasks[iter]['project']:
+                   #     parentproj = alltasks[iter]['projects'].split('.')[0]
+                    allprojects.append(alltasks[iter]['project'])
+            except KeyError:
+                pass
+            iter += 1
+        return allprojects
 
 
 def main():
     myprojects = Task()
     app = Flask(__name__)
 
-    projects = sorted(myprojects.get_projects())
+    projects = sorted(myprojects.get_all_projects(), key=lambda s: s.lower())
+    #projects = sorted(myprojects.get_all_projects())
     @app.route('/')
     def home():
         return render_template('index.html', projects=projects)
-    app.run()
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
